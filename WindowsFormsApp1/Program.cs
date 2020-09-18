6e1b8808a1cf616b9e094c7dcd660e203c2d1dd5
Farshid Mohammadi <fm7450@outlook.com>
2020-09-18 12:16:13 +0430

    990628

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,8 +17,33 @@ namespace SchoolApp
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            // var container = new StructureMap.Container(new TypeRegistery());
+            if (args != null && args.Contains("encrypt-config"))
+            {
+                if (args.Length < 2 || args[1] != "708801298") return;
+                var config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+                var connectionStringSection = config.GetSection("connectionStrings") as ConnectionStringsSection;
+                if (connectionStringSection != null && !connectionStringSection.SectionInformation.IsProtected)
+                {
+                    connectionStringSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+                }
+                config.Save();
+                return;
+            }
+            if (args != null && args.Contains("decrypt-config"))
+            {
+                if (args.Length < 2 || args[1] != "708801298") return;
+                var config = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+                var connectionStringSection = config.GetSection("connectionStrings") as ConnectionStringsSection;
+                if (connectionStringSection != null && connectionStringSection.SectionInformation.IsProtected)
+                {
+                    connectionStringSection.SectionInformation.UnprotectSection();
+                }
+                config.Save();
+                return;
+            }
             Thread.CurrentThread.CurrentCulture = new CultureInfo("fa-IR");
             Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
             Application.EnableVisualStyles();
